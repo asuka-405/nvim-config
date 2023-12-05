a188d5a21723296e020d6509f7371966ef1a531e
Suryansh Kapil <suryanshkapil405@gmail.com>
2023-12-05 17:50:41 +0000

    On branch main  Initial commit  Changes to be committed: 	new file:   after/plugin/colors.lua 	new file:   after/plugin/fugitive.lua 	new file:   after/plugin/harpoon.lua 	new file:   after/plugin/telescope.lua 	new file:   after/plugin/treesitter.lua 	new file:   after/plugin/undotree.lua 	new file:   init.lua 	new file:   lua/asuka/init.lua 	new file:   lua/asuka/packer.lua 	new file:   lua/asuka/remap.lua 	new file:   pack/packer/start/packer.nvim 	new file:   plugin/packer_compiled.lua

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,15 @@
+require'nvim-treesitter.configs'.setup {
+
+  ensure_installed = { "javascript", "typescript" , "c", "lua", "vim", "vimdoc", "query", "rust", "java" },
+
+  sync_install = false,
+
+  auto_install = true,
+
+  highlight = {
+
+    enable = true,	  
+    additional_vim_regex_highlighting = false,
+
+  },
+}
---
+++ init.lua
@@ -0,0 +1,7 @@
+require("asuka")
+
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
